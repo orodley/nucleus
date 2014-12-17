5448981fc8a94c739472007f666fa957ec66bcb8
Owen Rodley <owen.rodley@gmail.com>
2014-12-17 19:49:57 +1300

    The start of the bootstrap compiler.

--- boot/boot.asd
+++ boot/boot.asd
@@ -1,3 +1,8 @@
+;; Get ASDF to load the version in this directory, regardless of whether
+;; there's some other random one elsewhere in the filesystem
+(let ((asdf:*central-registry* (directory "CL-LLVM")))
+  (asdf:load-system 'llvm))
+
 (asdf:defsystem :boot
   :description "Bootstrap compiler for nucleus, implemented in Common Lisp."
   :components ((:file "package")
--- boot/src/boot.lisp
+++ boot/src/boot.lisp
@@ -1,6 +1,56 @@
 (in-package :boot)
 
-(defun test ()
-  (llvm:with-objects ((builder llvm:builder)
-                      (mod llvm:module "test"))
-    (llvm:dump-module mod)))
+(declaim (optimize debug))
+
+(defvar *builder*)
+(defvar *module*)
+
+(defvar *lisp-value* (llvm:int-type 64))
+(defvar *lowtag-bits* 4)
+
+(defun nuc-compile-file (input-filename output-filename)
+  (llvm:with-objects ((*builder* llvm:builder)
+                      (*module* llvm:module "test"))
+    (dolist (form (read-file input-filename))
+      (compile-toplevel-form form)) 
+    (llvm:write-bitcode-to-file *module* output-filename)))
+
+(defun compile-toplevel-form (form)
+  (ecase (car form)
+    (|defun|
+      (compile-defun (cadr form) (caddr form) (cdddr form)))))
+
+(defun compile-defun (name args body)
+  (let* ((func-type (llvm:function-type
+                     *lisp-value*
+                     (make-array (length args) :initial-element *lisp-value*)))
+         (func (llvm:add-function *module* (string name) func-type)))
+    (map nil
+         (lambda (param name)
+           (setf (llvm:value-name param) name))
+         (llvm:params func)
+         args)
+    (llvm:position-builder-at-end *builder* (llvm:append-basic-block func "entry"))
+    (loop for cons on body
+          for compiled-expr = (compile-expr (car cons))
+          when (null (cdr cons))
+            do (llvm:build-ret *builder* compiled-expr))
+    (unless (llvm:verify-function func)
+      (error "Invalid definition for function ~S" name))
+    (llvm:dump-value func)))
+
+(defun compile-expr (form)
+  (typecase form
+    ;; TODO: limit on size
+    (integer (llvm:const-int *lisp-value*
+                             (format nil "~D" (ash form *lowtag-bits*))
+                             10))))
+
+(defun read-file (filename)
+  (let ((eof-value (gensym))
+        (*readtable* (copy-readtable *readtable*)))
+    (setf (readtable-case *readtable*) :preserve)
+    (with-open-file (file filename)
+      (loop for form = (read file nil eof-value)
+            until (eq form eof-value)
+              collect form))))
