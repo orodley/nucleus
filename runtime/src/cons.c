02813ebcaa7afa340dc44d95870bd2c134ba7247
Owen Rodley <owen.rodley@gmail.com>
2015-01-05 03:23:21 +1300

    List reading.

--- runtime/src/cons.c
+++ runtime/src/cons.c
@@ -12,3 +12,21 @@ nuc_val rt_cons(nuc_val car, nuc_val cdr)
 
 	return ((nuc_val)c) | CONS_LOWTAG;
 }
+
+nuc_val rt_set_car(nuc_val cons, nuc_val new_car)
+{
+	// TODO: type checking
+	Cons *c = (Cons *)REMOVE_LOWTAG(cons);
+	c->car = new_car;
+
+	return new_car;
+}
+
+nuc_val rt_set_cdr(nuc_val cons, nuc_val new_cdr)
+{
+	// TODO: type checking
+	Cons *c = (Cons *)REMOVE_LOWTAG(cons);
+	c->cdr = new_cdr;
+
+	return new_cdr;
+}
--- runtime/src/misc.c
+++ runtime/src/misc.c
@@ -1,9 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 #include "nuc.h"
 
 nuc_val rt_panic(nuc_val message)
 {
-	(void)message;
+	String *str = (String *)REMOVE_LOWTAG(message);
+	fputs("(panic! \"", stderr);
+	fwrite(str->bytes, 1, str->length, stderr);
+	fputs("\")\n", stderr);
 
-	assert(!"Panic!");
+	exit(-1);
 }
